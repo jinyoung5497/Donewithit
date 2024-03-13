88b55cfdcb0fd47713ffa3026036ae9b44600abb
Jinyoung Choi <jinyoung5497@gmail.com>
2024-03-13 19:59:26 +1000

    test v1 001

--- expo-updates/App.js
+++ expo-updates/App.js
@@ -2,11 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log('App loaded!');
-
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on my app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
